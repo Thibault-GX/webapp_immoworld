3824444c3d143a4e4f6ef3283c237646070da07f
Thomas CARBUCCIA <thomas.carbuccia@axians.com>
2021-03-03 13:02:03 +0100

    Ajout fonction showHeader

--- src/App.js
+++ src/App.js
@@ -1,29 +1,56 @@
 import React from "react";
-import logo from './logo.svg';
 import './App.css';
 import {createBrowserHistory} from "history";
-import {Route, Router, Switch} from "react-router-dom";
+import {Route, Router, Switch, withRouter} from "react-router-dom";
 
 import Home from "./views/Home/Home.js";
-
-import Navbar from "./components/Navbar/Navbar";
-
+import Login from "./views/Login/Login";
+import Header from "./components/Header/Header";
 
 const hist = createBrowserHistory();
 
-function App() {
-  return (
-    <div className="App">
-        <header>
-            <Navbar />
-        </header>
-      <Router history={hist}>
-        <Switch>
-          <Route path="/" component={Home} />
-        </Switch>
-      </Router>
-    </div>
-  );
+const Routes = withRouter((props) => {
+    /**
+     * Permet d'afficher le header si la route actuelle n'est pas dans le tableau.
+     *
+     * @returns {JSX.Element}
+     */
+    const showHeader = () => {
+        const hiddenInRoutes = ['/login'];
+        const {pathname} = props.location;
+        if (!hiddenInRoutes.includes(pathname)) {
+            return (<Header/>);
+        }
+    }
+
+    // Retourne les différentes routes et le header
+    return (
+        <div>
+            {showHeader()}
+            <main>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/login" component={Login}/>
+                </Switch>
+            </main>
+        </div>
+    );
+})
+
+/**
+ * Elément Root
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const App = () => {
+    return (
+        <div className="App">
+            <Router history={hist}>
+                <Routes/>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,18 @@
 import React, {useState} from "react";
-import NavbarLinks from "./navbar-links/navbar-links";
+import './Navbar.css';
 
 export default function Navbar(props) {
     const [links] = useState([{
-        name: 'Thibault',
-        link: 'https://google.com',
+        name: 'Accueil',
+        link: '#',
     }, {
-        name: 'Brian',
-        link: 'https://soluclef.com',
+        name: 'Agenda',
+        link: '#',
     }]);
 
     return (
-        <div style={{width: '100%', height: '50px', backgroundColor: '#FAFAFA'}}>
-            <NavbarLinks links={links}/>
+        <div className="Navbar">
+
         </div>
     )
 }
