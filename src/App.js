d7343756e7fed7492d8c288e77e6670e685beeab
Thomas CARBUCCIA <thomas.carbuccia@axians.com>
2021-03-03 14:35:00 +0100

    Modification App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import './App.css';
 import {createBrowserHistory} from "history";
 import {Route, Router, Switch, withRouter} from "react-router-dom";
