0d5d5c88d610f25532a4e6a028ab053652309dd2
Thomas CARBUCCIA <thomas.carbuccia@axians.com>
2021-03-03 13:18:54 +0100

    exemple lien

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,9 +1,8 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 export default function Home(props) {
     return (
-        <div>
-            Hello World from home
-        </div>
+            <Link to={{pathname: '/login'}}>Go To Login</Link>
     )
 }
