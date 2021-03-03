9668d73a47074d3efd17ac19bdfa107151d367db
Brian Fontaine <55120580+BrianFontaine@users.noreply.github.com>
2021-03-03 13:03:39 +0100

    rr

--- src/App.js
+++ src/App.js
@@ -2,24 +2,31 @@ import React from "react";
 import logo from './logo.svg';
 import './App.css';
 import {createBrowserHistory} from "history";
-import {Route, Router, Switch} from "react-router-dom";
+import {Route, Router, Switch, useLocation} from "react-router-dom";
 
 import Home from "./views/Home/Home.js";
 
 import Navbar from "./components/Navbar/Navbar";
+import Login from "./views/Login/Login";
 
 
 const hist = createBrowserHistory();
 
+const navbarView = () => {
+  const location = useLocation();
+  console.log(location.pathname);
+}
 function App() {
   return (
     <div className="App">
-        <header>
-            <Navbar />
-        </header>
+      <header>
+          {navbarView}
+          <Navbar />
+      </header>
       <Router history={hist}>
         <Switch>
-          <Route path="/" component={Home} />
+          <Route path="/login" component={Login}/>
+          <Route exact path="/" component={Home} />
         </Switch>
       </Router>
     </div>
