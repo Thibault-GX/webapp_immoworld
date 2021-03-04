8222087e965a9c6059b6ce2074cff48278ff85e4
Brian Fontaine <55120580+BrianFontaine@users.noreply.github.com>
2021-03-04 16:40:15 +0100

    rien

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -3,11 +3,10 @@ import {Route, Redirect} from "react-router-dom";
 import {useAuth} from "../../context/auth";
 
 export default function PrivateRoute({component: Component, ...rest}) {
-    const {isAuth} = useAuth();
     const route = <Route {...rest} render={(props) => (
         <Component {...props} />
     )}/>;
     return (
-        isAuth ? route : <Redirect to="/login" />
+        useAuth() ? route : <Redirect to="/login" />
     )
 }
